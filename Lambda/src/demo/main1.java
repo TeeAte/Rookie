0e493fe953c4950b4f254a195a77c0efb96a636b
TeeAte <Luwenkeng@foxmail.com>
2025-08-10 16:27:45 +0800

    nextStep

--- Lambda/src/demo/main1.java
+++ Lambda/src/demo/main1.java
@@ -1,20 +1,41 @@
 package demo;
 
 public class main1 {
-
+//lambda表达式即函数式接口
     public static void main(String[] args) {
-    Animal A =new Animal(){
+
+    Animal A =new Animal() {
         @Override
         public void cry() {
-            System.out.println("miao~");
+            System.out.println("miao~~");
         }
+    };
+    A.cry();
+
+    //lambda表达式
+    Animal A1=()-> {
+        System.out.println("miao~~miao~~");
+    };
+    A1.cry();
+
+
+    //简化
+    DOG A2 = a -> System.out.println("miao~~miao~~miao" + a);
+    A2.cry(20);
     }
 
+
+
+
+
+    @FunctionalInterface//声明函数式接口
+    interface Animal{
+        public abstract void cry();//只可以接口，一个方法
     }
 
-    abstract static class Animal{
-
-        public abstract void cry();
+    @FunctionalInterface//声明函数式接口
+    interface DOG{
+        public abstract void cry(int a);//只可以接口，一个方法
     }
 
 }
