5a50a6ad4679f2c67420d6192cffddd21b90423c
TeeAte <luwenkeng@foxmail.com>
2025-08-15 22:52:14 +0800

    OK

---
+++ Error/src/d1/main1.java
@@ -0,0 +1,30 @@
+package d1;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class main1 {
+    public static void main(String[] args) throws ParseException {
+        show("2025-05-15 22:27:44");
+
+        try {//相当于监视异常
+            show("2025-05-15 ");
+        }
+        catch (ParseException e) {//捕获异常
+            System.out.println("程序出错了！");
+        }
+    }
+
+    public static void show(String str) throws ParseException {//抛出异常
+        System.out.println("\n"+"===程序开始===");
+        // 编译异常：编译阶段报错，编译不通过。
+        // 把字符串时间解析成 Java 中的一个日期对象。
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = sdf.parse(str); // 编译时异常，提醒程序员这里的程序很容易出错，请您注意！
+        System.out.println(date);
+
+        System.out.println("==程序结束==");
+    }
+
+}
